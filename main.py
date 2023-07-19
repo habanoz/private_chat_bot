c63914d5f4a9ace080b6914ac35529f52b50452b
habanoz <huseyinabanox@gmail.com>
2023-07-19 11:28:27 +0300

    generation pipeline.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import langchain
 from langchain import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 from scripts.bot.loader.doc_csv_loader import DocCsvLoder
 from scripts.bot.retriever.sklearn_retriever import SKLearnRetriever
@@ -31,18 +31,32 @@ def main():
     model = model.eval()
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
+    generation_config = model.generation_config
+    generation_config.temperature = 0
+    generation_config.num_return_sequences = 1
+    generation_config.max_new_tokens = 256
+    generation_config.use_cache = False
+    generation_config.repetition_penalty = 1.7
+    generation_config.pad_token_id = tokenizer.eos_token_id
+    generation_config.eos_token_id = tokenizer.eos_token_id
+
     stop_tokens = ['Question:', '<human>:', '<bot>:']
     stop_token_ids = [tokenizer.encode(stop_token, add_special_tokens=False)[0] for stop_token in stop_tokens]
 
-    llm = HuggingFacePipeline(model=model,
-                              tokenizer=tokenizer,
-                              return_full_text=True,
-                              task="text-generation",
-                              model_kwargs={"temperature": 0,
-                                            # "max_length": 1024,
-                                            # "eos_token_id": stop_token_ids
-                                            }
-                              )
+    generation_pipeline = pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=True,
+        task="text-generation",
+        # stopping_criteria=stopping_criteria,
+        generation_config=generation_config,
+        odel_kwargs={"temperature": 0,
+                     # "max_length": 1024,
+                     # "eos_token_id": stop_token_ids
+                     }
+    )
+
+    llm = HuggingFacePipeline(pipeline=generation_pipeline)
 
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     answer = qa.run(question)
