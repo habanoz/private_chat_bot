fa0a51b9393da3f347c3710d591786d56f2093d5
habanoz <huseyinabanox@gmail.com>
2023-07-19 12:56:02 +0300

    modify prompt.

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import os
 from pathlib import Path
 
 import langchain
-from langchain import HuggingFacePipeline, PromptTemplate
+from langchain import HuggingFacePipeline, PromptTemplate, OpenAI
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 from scripts.bot.loader.doc_csv_loader import DocCsvLoder
 from scripts.bot.retriever.sklearn_retriever import SKLearnRetriever
+import os
 
 langchain.debug = True
 os.environ['DEBUG'] = 'True'
+os.environ['OPENAI_API_KEY'] = ""
 
 MODEL_NAMES = ["togethercomputer/RedPajama-INCITE-Chat-3B-v1", "tiiuae/falcon-7b-instruct"]
-MODEL_NAME = MODEL_NAMES[1]
+MODEL_NAME = MODEL_NAMES[0]
 
 
 def main():
@@ -25,12 +27,30 @@ def main():
 
     question = "What areas did Beyonce compete in when she was growing up?"
 
+    llm = get_hf_model(MODEL_NAME)
+    # llm = OpenAI()
+
+    prompt_template = """Use the following pieces of context delimited by triple backticks to answer the question at the end. If the context does not contain the answer, do not try to generate answer, just say that the provided context does not include the answer.
+
+    Context: ```{context}```
+
+    Question: {question}
+    Answer:"""
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs = {"prompt": prompt})
+    answer = qa.run(question)
+
+    print(answer)
+
+
+def get_hf_model(model_name:str):
     model = AutoModelForCausalLM.from_pretrained(
-        MODEL_NAME, trust_remote_code=True, load_in_8bit=True, device_map="auto"
+        model_name, trust_remote_code=True, load_in_8bit=True, device_map="auto"
     )
     model = model.eval()
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     generation_config = model.generation_config
     generation_config.temperature = 0
     generation_config.num_return_sequences = 1
@@ -39,10 +59,8 @@ def main():
     generation_config.repetition_penalty = 1.7
     generation_config.pad_token_id = tokenizer.eos_token_id
     generation_config.eos_token_id = tokenizer.eos_token_id
-
     stop_tokens = ['Question:', '<human>:', '<bot>:']
     stop_token_ids = [tokenizer.encode(stop_token, add_special_tokens=False)[0] for stop_token in stop_tokens]
-
     generation_pipeline = pipeline(
         model=model,
         tokenizer=tokenizer,
@@ -51,26 +69,12 @@ def main():
         # stopping_criteria=stopping_criteria,
         generation_config=generation_config,
         model_kwargs={"temperature": 0,
-                     # "max_length": 1024,
-                     # "eos_token_id": stop_token_ids
-                     }
+                      # "max_length": 1024,
+                      # "eos_token_id": stop_token_ids
+                      }
     )
 
-    llm = HuggingFacePipeline(pipeline=generation_pipeline)
-
-    prompt_template = """Use the following pieces of context to answer the question at the end. If the context don't contain the answer, just say that the provided context does not include the answer.
-
-    {context}
-
-    Question: {question}
-    Helpful Answer:"""
-    prompt = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs = {"prompt": prompt})
-    answer = qa.run(question)
-
-    print(answer)
+    return HuggingFacePipeline(pipeline=generation_pipeline)
 
 
 if __name__ == '__main__':
